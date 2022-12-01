b3bd2605a0a656330d8a9552d231d61de5486566
debabrotos <debabroto58@gmail.com>
2022-12-01 23:40:07 +0600

    ok

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,7 +5,7 @@
   <div class="card-body">
     <blockquote class="blockquote mb-0">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+      <footer class="blockquote-footer">Someone asdasdasdsfsfsafamous in <cite title="Source Title">Source Title</cite></footer>
     </blockquote>
   </div>
 </div>
