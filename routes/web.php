7567b484957c693d2928020cea0f45dd6765fb17
debabrotos <debabroto58@gmail.com>
2023-01-17 20:53:53 +0600

    FINAL PUSH 0

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,12 @@ Route::get('/logout', function () {
     Session::forget('user');
     return redirect('login');
 });
+Route::view('/register','register');
 
 Route::post("/login",[UserController::class,'login']);
+Route::post("/register",[UserController::class,'register']);
+
+
 Route::get("/",[ProductController::class,'index']); 
 Route::get("detail/{id}",[ProductController::class,'detail']); 
 Route::post("add_to_cart",[ProductController::class,'addToCart']); 
@@ -34,3 +38,4 @@ Route::get("removecart/{id}",[ProductController::class,'removeCart']);
 
 Route::get("ordernow",[ProductController::class,'orderNow']);
 Route::post("orderplace",[ProductController::class,'orderPlace']);
+Route::get("myorders",[ProductController::class,'myOrders']);
