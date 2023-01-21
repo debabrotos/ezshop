2e7cf6a28d8e61c29153d255f21969d88b2288f1
debabrotos <debabroto58@gmail.com>
2023-01-22 01:18:09 +0600

    Final - header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,46 +7,94 @@ if(Session::has('user'))
 }
 
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">EZShop</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<div class="header_nav">
+  <nav class="navbar navbar-expand-lg nav_content">
+    <a class="navbar-brand nav_logo" href="/">EZShop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
-      @if(Session::has('user'))
-        <li class="nav-item"><a class="nav-link" href="/myorders">Order</a></li>
-      @endif
-
-      <!-- <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form> -->
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="/cartlist">Cart({{$total}})</a></li>
-
-      <div style="margin-left:10px" class="dropdown_header">
+    <div class="collapse navbar-collapse nav_items" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
         @if(Session::has('user'))
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
-          <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="/logout">Logout</a></li>
-          </ul>
-        </li>
-        @else
-        <div class="user_panel">
-            <li><a href="/login">Login</a>
-                <a href="/register">Register</a>
-            </li>
-        </div>
+          <li class="nav-item"><a class="nav-link" href="/myorders">Order</a></li>
         @endif
-      </div>
-      
 
-    </ul>
-  </div>
-</nav>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+      </ul>
+      <ul class="nav navbar-nav navbar-right nav_right_panel">
+        <li><a href="/cartlist">Cart({{$total}})</a></li>
+
+        <div class="dropdown_header">
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu custom_dropdown_menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <div class="user_panel">
+              <li><a href="/login">Login</a>
+                  <a href="/register">Register</a>
+              </li>
+          </div>
+          @endif
+        </div>
+        
+
+      </ul>
+    </div>
+  </nav>
+</div>
+
+
+<!-- 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand " href="/">EZShop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
+        @if(Session::has('user'))
+          <li class="nav-item"><a class="nav-link" href="/myorders">Order</a></li>
+        @endif
+
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/cartlist">Cart({{$total}})</a></li>
+
+        <div style="margin-left:10px" class="dropdown_header">
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <div class="user_panel">
+              <li><a href="/login">Login</a>
+                  <a href="/register">Register</a>
+              </li>
+          </div>
+          @endif
+        </div>
+        
+
+      </ul>
+    </div>
+  </nav> -->
