dcae524278f0a8da9fb515727ecd2c9088a106dd
debabrotos <debabroto58@gmail.com>
2023-01-26 17:35:22 +0600

    last on master

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <div style=" bottom: 0px; left: 0px; right: 0px; width: 100%;" class="card">
-  <div style ="clear:both ;background-color: grey;"class="card-header">
-    This is footer 
+  <div style ="clear:both ;background-color: #169CC9;"class="card-header">
+    <p>This is Footer</p>
   </div>
   <!-- <div class="card-body">
     <blockquote class="blockquote mb-0">
