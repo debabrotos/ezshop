ff7c1e8d6f0cda3d4bcc35cfb24e0630e314c1cc
debabrotos <debabroto58@gmail.com>
2023-01-26 18:38:06 +0600

    register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,25 +2,35 @@
 @section("content")
 
 <div class="container custom-login">
-    <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
-            <form action="register" method="POST" >
-                @csrf 
-                <div class="form-group">
-                    <label for="exampleInputEmail1">User Name</label>
-                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="User Name">
+    <div class="myCard1">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-sm-4 col-sm-offset-4 myCard">
+                <div class="myLeftCtn">
+                    <form action="register" method="POST" class="myForm text-center" >
+                        <header>Register</header>    
+                    @csrf 
+                        <div class="form-group">
+                        <i class="fas fa-user"></i>
+                            <!-- <label for="exampleInputEmail1">User Name</label> -->
+                            <input type="text" name="name" class="myInput" id="exampleInputEmail1" placeholder="User Name">
+                        </div>
+                        <div class="form-group">
+                            <i class="fas fa-envelope"></i>
+                            <!-- <label for="exampleInputEmail1">Email address</label> -->
+                            <input type="email" name="email" class="myInput" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+                        </div>
+                        <div class="form-group">
+                            <i class="fas fa-lock"></i>
+                            <!-- <label for="exampleInputPassword1">Password</label> -->
+                            <input type="password" name="password" class="myInput" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="butt">Submit</button>
+                    </form>
+
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Register</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
